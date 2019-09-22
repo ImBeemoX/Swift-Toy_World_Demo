500245a203711760973d23c2a5bcc6037653ca69
Yichuan Wang <w369371@gmail.com>
2019-09-22 18:06:11 +1000

    Initial Commit

---
+++ Toy world/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Toy world
+//
+//  Created by 王一川 on 22/9/19.
+//  Copyright © 2019 Wang Yichuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
